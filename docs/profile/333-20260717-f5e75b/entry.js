d34ca496cfd20a9723831df9e9c5cca000af8ee5
Amashiro Natsuki <amashiro.natsuki0796@gmail.com>
2026-07-17 21:46:09 +0800

    Add memorial entry 333-20260717-f5e75b

---
+++ docs/profile/333-20260717-f5e75b/entry.js
@@ -0,0 +1,25 @@
+const entry = {
+  "age": 12,
+  "biography": "",
+  "birthDate": "",
+  "date": "2026-08-08",
+  "deathDate": "2026-08-08",
+  "gender": "不便公开",
+  "hobbies": [],
+  "id": "sub_20260717_134558_1591e849",
+  "name": "333额度",
+  "nickname": "333额度",
+  "occupation": "",
+  "passingCause": "暂未公开详细信息。",
+  "petalsEnabled": true,
+  "quote": "",
+  "region": "",
+  "socials": [],
+  "summary": "",
+  "tags": [],
+  "useDefaultBackground": true
+}
+
+entry.photo = new URL('./avatar.jpg', import.meta.url).href
+
+export default entry
---
+++ docs/profile/333-20260717-f5e75b/index.md
@@ -0,0 +1,7 @@
+---
+title: "333额度"
+layout: doc
+pageClass: wide-doc-page
+---
+
+<MemorialEntryDetail slug="333-20260717-f5e75b" />
