030e43590dbf4dd209629bf2156c4233e0241904
Amashiro Natsuki <amashiro.natsuki0796@gmail.com>
2026-07-19 08:51:42 +0800

    Add memorial entry xi-20260719-ad2e04

---
+++ docs/profile/xi-20260719-ad2e04/entry.js
@@ -0,0 +1,25 @@
+const entry = {
+  "age": 22,
+  "biography": "",
+  "birthDate": "",
+  "date": "2026-08-03",
+  "deathDate": "2026-08-03",
+  "gender": "女性",
+  "hobbies": [],
+  "id": "sub_20260719_005131_e369b4f3",
+  "name": "xi",
+  "nickname": "xi",
+  "occupation": "",
+  "passingCause": "暂未公开详细信息。",
+  "petalsEnabled": true,
+  "quote": "",
+  "region": "",
+  "socials": [],
+  "summary": "",
+  "tags": [],
+  "useDefaultBackground": true
+}
+
+entry.photo = new URL('./avatar.jpg', import.meta.url).href
+
+export default entry
---
+++ docs/profile/xi-20260719-ad2e04/index.md
@@ -0,0 +1,7 @@
+---
+title: "xi"
+layout: doc
+pageClass: wide-doc-page
+---
+
+<MemorialEntryDetail slug="xi-20260719-ad2e04" />
