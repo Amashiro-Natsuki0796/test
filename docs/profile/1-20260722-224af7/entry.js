7caad8ae35d7fa5d84241dac51ec74f85016bc5a
Amashiro Natsuki <amashiro.natsuki0796@gmail.com>
2026-07-22 17:35:29 +0800

    Add memorial entry 1-20260722-224af7

---
+++ docs/profile/1-20260722-224af7/entry.js
@@ -0,0 +1,26 @@
+const entry = {
+  "age": 12,
+  "biography": "",
+  "birthDate": "",
+  "date": "2026-08-08",
+  "deathDate": "2026-08-08",
+  "gender": "女性",
+  "hobbies": [],
+  "id": "sub_20260722_093518_4e534269",
+  "name": "1",
+  "nickname": "1",
+  "occupation": "",
+  "passingCause": "暂未公开详细信息。",
+  "petalsEnabled": true,
+  "quote": "",
+  "region": "",
+  "socials": [],
+  "summary": "",
+  "tags": [],
+  "uploadedAt": "2026-07-22T09:35:18Z",
+  "useDefaultBackground": true
+}
+
+entry.photo = new URL('./avatar.png', import.meta.url).href
+
+export default entry
