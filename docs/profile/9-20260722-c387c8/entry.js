3bba2722c25cbcafac7aae54d2dde7f217e84f32
Amashiro Natsuki <amashiro.natsuki0796@gmail.com>
2026-07-22 20:47:35 +0800

    Add memorial entry 9-20260722-c387c8

---
+++ docs/profile/9-20260722-c387c8/entry.js
@@ -0,0 +1,30 @@
+const entry = {
+  "age": 99,
+  "biography": "傻逼",
+  "birthDate": "待补充",
+  "date": "2026-08-08",
+  "deathDate": "2026-08-08",
+  "gender": "女性",
+  "hideBiographyTitle": true,
+  "hobbies": [
+    "待补充"
+  ],
+  "id": "sub_20260722_124724_820ca7ba",
+  "name": "9",
+  "nickname": "9",
+  "occupation": "待补充",
+  "petalsEnabled": true,
+  "quote": "待补充",
+  "region": "待补充",
+  "socials": [],
+  "summary": "傻逼",
+  "tags": [
+    "待补充"
+  ],
+  "uploadedAt": "2026-07-22T12:47:24Z",
+  "useDefaultBackground": true
+}
+
+entry.photo = new URL('./avatar.png', import.meta.url).href
+
+export default entry
---
+++ docs/profile/9-20260722-c387c8/index.md
@@ -0,0 +1,7 @@
+---
+title: "9"
+layout: doc
+pageClass: wide-doc-page
+---
+
+<MemorialEntryDetail slug="9-20260722-c387c8" />
