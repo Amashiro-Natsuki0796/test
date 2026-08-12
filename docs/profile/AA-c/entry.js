113db957134e06d3cbe5153287136bbb91b2a734
Amashiro Natsuki <amashiro.natsuki0796@gmail.com>
2026-08-13 00:01:20 +0800

    Add memorial entry AA-c

---
+++ docs/profile/AA-c/entry.js
@@ -0,0 +1,30 @@
+const entry = {
+  "age": 18,
+  "biography": "待补充",
+  "birthDate": "待补充",
+  "date": "2222-11-11",
+  "deathDate": "2222-11-11",
+  "gender": "男性",
+  "hobbies": [
+    "ddddddd"
+  ],
+  "id": "sub_20260812_160107_2f4e795a",
+  "name": "小星 小小",
+  "nickname": "小星 小小",
+  "occupation": "1122s",
+  "petalsEnabled": true,
+  "quote": "待补充",
+  "region": "湖南省",
+  "socials": [],
+  "summary": "待补充",
+  "tags": [
+    "ddddddd"
+  ],
+  "uploadedAt": "2026-08-12T16:01:07Z",
+  "useDefaultBackground": true,
+  "websites": []
+}
+
+entry.photo = new URL('./avatar.png', import.meta.url).href
+
+export default entry
---
+++ docs/profile/AA-c/index.md
@@ -0,0 +1,7 @@
+---
+title: "小星 小小"
+layout: doc
+pageClass: wide-doc-page
+---
+
+<MemorialEntryDetail slug="AA-c" />
